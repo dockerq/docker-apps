0b311a59c9c2e3c5ffccd8d22731358796f0329f
adolphlwq <kenan3015@gmail.com>
2021-05-18 14:20:42 +0800

    add redis config

--- redis/start.sh
+++ redis/start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 docker run -d --name redis --net host \
-    -v `pwd`/etc/redis:/usr/local/etc/redis/redis.conf
+    -v `pwd`/etc/redis.conf:/usr/local/etc/redis/redis.conf
     redis:latest
     # redis:6.2-alpine
