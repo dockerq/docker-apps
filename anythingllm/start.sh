4522dd44617771ef169487a06be8a1c2b1074e81
alwqx <kenan3015@gmail.com>
2024-05-07 20:42:57 +0800

    anythingllm inti

---
+++ anythingllm/start.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+docker run -d --name=allm -p 3001:3001 \
+    --cap-add SYS_ADMIN \
+    --add-host=host.docker.internal:host-gateway \
+    -v `pwd`/data:/app/server/storage \
+    -v `pwd`/.env:/app/server/.env \
+    -e STORAGE_DIR="/app/server/storage" \
+    mintplexlabs/anythingllm
