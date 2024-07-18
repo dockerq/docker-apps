e671403fe53125775d4c19602e3a978af87866c9
alwqx <kenan3015@gmail.com>
2024-07-18 21:54:04 +0800

    lobe-chat: add container restart policy

--- lobechat/start.sh
+++ lobechat/start.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-docker run -d --name lobechat -p 3210:3210 -e ACCESS_CODE=lobe666 lobehub/lobe-chat
+docker run -d --name lobechat --restart always \
+    -p 3210:3210 -e ACCESS_CODE=lobe666 \
+    lobehub/lobe-chat:v1.5.4
