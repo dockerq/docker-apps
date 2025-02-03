0c5a07e8edcc06ba23e40316a790ac910c8c173a
alwqx <kenan3015@gmail.com>
2025-02-03 16:32:06 +0800

    [lobechat] remove

--- lobechat/README.md
+++
@@ -1,3 +0,0 @@
-# LobeChat Docker Image
-
-refer [Docker 部署指引](https://lobehub.com/zh/docs/self-hosting/platform/docker)
--- lobechat/start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#-p 3210:3210 \
-
-docker run -d --name lobechat --net host --restart always \
-    -e ACCESS_CODE=lobe666 \
-    lobehub/lobe-chat:v1.7.2
